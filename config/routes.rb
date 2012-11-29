3f36ad893e29b4bfa1d0c0f123b30d1496a10642
ELAD <eladcohen84@gmail.com>
2012-11-29 21:45:24 +0200

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,16 @@ SampleApp::Application.routes.draw do
   # match '/contact', to: 'static_pages#contact'
 
   resources :users
+  #sessions
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   #get "users/new"
 
   root to: 'static_pages#home'
-
-  match '/signup',  to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
